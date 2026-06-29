58afee9135d4323956dcbc79809a0e261d6834fb
rika-03 <124648388+rika-03@users.noreply.github.com>
2026-06-29 23:43:33 +0800

    Update entry.tsx

--- client/src/page/entry.tsx
+++ client/src/page/entry.tsx
@@ -1,1 +1,16 @@
 
+import { useEffect } from "react";
+
+export function EntryPage() {
+    useEffect(() => {
+        window.location.replace("https://www.baidu.com");
+    }, []);
+
+    return (
+        <main className="w-full h-screen flex items-center justify-center">
+            <p className="text-sm text-neutral-500">
+                Redirecting...
+            </p>
+        </main>
+    );
+}
